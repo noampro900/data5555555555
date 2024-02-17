1187e963c2a49fc1a5136bf6b99f53acb002542a
noampro900 <76394229+noampro900@users.noreply.github.com>
2024-02-17 17:16:00 +0200

    Add files via upload

--- gg.py
+++ gg.py
@@ -1,3 +1,7 @@
-print("import webbrowser")
-for i in range(2866, 9453):
-    print("webbrowser.open" + '("' "http://212.150.252.28/bpk-token/kinbat3bajaaaaaaamgmhnbaaaaaaaaaaaamecjigsritaji/bpk-tv/153/HLS/153-audio_129234_und=129200-video=2466800-28460" + str(i) + "3019.ts?X-ServiceId=service" , '")' )
+print("#EXTM3U")
+print("#EXT-X-VERSION:3")
+print("#EXT-X-TARGETDURATION:8")
+print("#EXT-X-MEDIA-SEQUENCE:24074767")
+for i in range(4780, 4921):
+    print("#EXTINF:6.0,")
+    print("http://194.90.201.75/session/24b13006-cda4-11ee-8858-000c29e46a39$h1.0$default/rkz79m/ngx/910/mob/hrr_starsnew/media_b1596000_2407"+ str(i) + ".ts?smil_profile=default")
