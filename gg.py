c05bf33e2e112c089ea6629b0cafcac8387abfd3
noampro900 <76394229+noampro900@users.noreply.github.com>
2024-02-17 02:36:16 +0200

    Add files via upload

---
+++ gg.py
@@ -0,0 +1,3 @@
+print("import webbrowser")
+for i in range(2866, 9453):
+    print("webbrowser.open" + '("' "http://212.150.252.28/bpk-token/kinbat3bajaaaaaaamgmhnbaaaaaaaaaaaamecjigsritaji/bpk-tv/153/HLS/153-audio_129234_und=129200-video=2466800-28460" + str(i) + "3019.ts?X-ServiceId=service" , '")' )
